048155437d2d3b69846f96432dc9de913eea0c99
2016032897 <rlqja1107@hanyang.ac.kr>
2021-02-14 14:37:06 +0900

    blog

--- _js/src/push-state.js
+++ _js/src/push-state.js
@@ -46,10 +46,7 @@ const REQUIREMENTS = [
   'opacity',
   'cssanimations',
 ];
-<script type="text/x-mathjax-config">
-  MathJax.Hub.Config({ tex2jax: { inlineMath: [['$','$'], ['\\(','\\)']] } });
-      </script>
-<script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
+
 const DURATION = 250;
 const FADE_DURATION = 500;
 
